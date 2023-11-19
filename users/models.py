770bf0a081e1928c37220aeb76bdc48af08c3170
Uesto228 <tima.ismailov.02@mail.ru>
2023-11-19 18:33:47 +0600

    added vehicle model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields.related import ForeignKey
 from django.utils import timezone
 
 
@@ -24,6 +25,19 @@ class Driver(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     # TODO: ROUTE, VEHICLE
+
+
+class Vehicle(models.Model):
+    driver = ForeignKey(Driver, on_delete=models.CASCADE)
+    status = models.BooleanField()                          # 1 - active, 0 - inactive
+    manufacturer = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    year = models.IntegerField()
+    license_plate = models.CharField(max_length=10)
+    capacity = models.IntegerField()
+    mileage = models.IntegerField()
+    cost = models.IntegerField()
 
 
 class MaintenancePerson(models.Model):
