3887533b7c44a2aa2d87b399515b37e71fb49473
Uesto228 <tima.ismailov.02@mail.ru>
2023-11-28 20:41:34 +0600

    create fuel task button

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import Auction, AuctionImage, Route, User, Driver, FuelPreson, MaintenancePerson, Vehicle
+from .models import Auction, AuctionImage, FuelReport, Route, User, Driver, FuelPreson, MaintenancePerson, Vehicle
 
 admin.site.register(User)
 admin.site.register(Driver)
@@ -11,4 +11,4 @@ admin.site.register(Vehicle)
 admin.site.register(Route)
 admin.site.register(Auction)
 admin.site.register(AuctionImage)
-admin.site.register(AuctionImage)
+admin.site.register(FuelReport)
